e64f3fc945fb59fd6d13eb3645516c270b7c4912
Juan Carlos Salcedo <correojuancarlos2014@gmail.com>
2026-07-27 21:12:27 -0600

    fix: actualizar permisos del bot de Discord en URL de invitacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   production: false,
 
   // Link de invitación del bot de Discord (OAuth2)
-  discordBotInviteUrl: 'https://discord.com/oauth2/authorize?client_id=1529727711390466200&permissions=8&integration_type=0&scope=bot',
+  discordBotInviteUrl: 'https://discord.com/oauth2/authorize?client_id=1529727711390466200&permissions=66560&integration_type=0&scope=bot',
 };
